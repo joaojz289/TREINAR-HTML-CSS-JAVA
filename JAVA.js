dd6d6ca78421bc3775210852e6958aa7214dc9b5
joaojz289 <joaopauloelements@gmail.com>
2025-08-05 15:50:12 -0300

    Update JAVA.js

--- JAVA.js
+++ JAVA.js
@@ -1,1 +1,12 @@
+const player = document.getElementById("machado")
+let posicaoX = 0
+
+addEventListener('keydown', ad)
+function ad(event) {
+    if(event.key === 'd'){
+        posicaoX += 10
+        player.style.left = posicaoX + 'px'
+    }
+}
+
 
